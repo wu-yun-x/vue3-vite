6d89b51009e16ad73aa833d25b135f19bce3220e
wuyun <1939716743@qq.com>
2025-01-09 22:01:03 +0800

    feat: 添加注释以及内容

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <script setup>
-// import { ref } from 'vue';
-// import HelloWorld from './components/HelloWorld.vue'
-
-// const visible = ref(false);
-// const openDialog = () => {
-//     // eslint-disable-next-line no-undef
-//     ElMessageBox.alert('弹框内容');
-//     visible.value = true;
-// };
-// const closeDialog = () => {
-//     // eslint-disable-next-line no-undef
-//     ElMessageBox.success('操作成功');
-//     visible.value = false;
-// };
 </script>
 <template>
-    <!-- <HelloWorld msg="Vite + Vue" /> -->
     <router-view></router-view>
 </template>
 
--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: wuyun 1939716743@qq.com
+ * @Date: 2025-01-09 21:29:17
+ * @LastEditors: wuyun 1939716743@qq.com
+ * @LastEditTime: 2025-01-09 22:00:13
+ * @FilePath: \vue3-vite\src\layout\Index.vue
+ * @Description: tag视图
+-->
 <template>
     <div class="g-app-container">
         <AppHeader />
